95d906664cb96b1b1251e62c4ce11698ae019056
mhaddaoui <mhaddaou@student.1337.ma>
2022-10-28 14:48:51 -0400

    cpp06 is done

--- CPP06/ex02/src/Base.cpp
+++ CPP06/ex02/src/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: mhaddaou < mhaddaou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 03:49:54 by mhaddaou          #+#    #+#             */
-/*   Updated: 2022/10/28 04:03:06 by mhaddaou         ###   ########.fr       */
+/*   Updated: 2022/10/28 14:48:05 by mhaddaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 Base::~Base(){}
 
 Base * generate(void){
-    	switch (rand() % 3)
+	
+	srand(time(0));
+    int random = rand() % 3;
+		
+    	switch (random)
 	{
 	case 0:
 		return (new A());
@@ -30,4 +34,31 @@ Base * generate(void){
 		perror("Something went wrong with the random generator");
 		return (NULL);
 	}
+}
+
+void identify(Base* p){
+	if (dynamic_cast<A*>(p) != 0)
+		std::cout << "A" << std::endl;
+	if (dynamic_cast<B*>(p) != 0)
+		std::cout << "B" << std::endl;
+	if (dynamic_cast<C*>(p) != 0)
+		std::cout << "C" << std::endl;
+}
+
+void identify (Base& p){
+	A a;
+	B b;
+	C c;
+	try{
+		a = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}catch (std::exception& e)	{}
+	try{
+		b = dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+	}catch(std::exception& e)	{}
+	try {
+		c = dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+	}catch(std::exception& e){}
 }
--- CPP06/ex02/src/main.cpp
+++ CPP06/ex02/src/main.cpp
@@ -6,7 +6,26 @@
 /*   By: mhaddaou < mhaddaou@student.1337.ma>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 03:49:56 by mhaddaou          #+#    #+#             */
-/*   Updated: 2022/10/28 03:49:57 by mhaddaou         ###   ########.fr       */
+/*   Updated: 2022/10/28 13:55:52 by mhaddaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/Classes.hpp"
+#include "../includes/Base.hpp"
+
+int main (){
+        Base *ptr;
+        ptr = generate();
+        identify(ptr);
+        identify(*(ptr));
+
+
+
+
+
+
+        
+        return 0;
+
+    
+}
